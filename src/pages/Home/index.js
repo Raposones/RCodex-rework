c8ae200ef23f4d2eb927dcde0f1ab586728f1726
Raposones <raphaelgarcia@alu.ufc.br>
2023-03-16 22:13:47 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import { Header } from '../../components/Header/index.js'
+
+const App = () => {
+    return (
+        <div className="App">
+            <Header />
+            <div className='champions_list'>
+                
+            </div>
+        </div>
+    );
+};
+
+export default App;
